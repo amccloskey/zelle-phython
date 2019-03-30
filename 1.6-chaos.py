782c9e48c50bfdce9dda68b119215eb9e2d9b6c6
Alexander McCloskey <alexander.mccloskey@gmail.com>
2019-03-31 02:14:39 +0800

    Exercise 3

--- 1.6-chaos.py
+++ 1.6-chaos.py
@@ -5,8 +5,9 @@ def main():
     print("The program illustrates a chaotic function")
     x = eval(input("Enter the first number between 0 and 1: "))
     n = eval(input("How many numbers should I print? "))
+    k = eval(input("What seed should I use? "))
     for i in range (n):
-        x = 3.9 * x * (1-x)
+        x = k * x * (1-x)
         print(x)
 
 main()
