85fc9295e3248376805cc15825ba18807aa7b518
Alexander <Alexander@AM-FHP.local>
2019-03-31 02:08:18 +0800

    Exercise 5

--- 1.6-chaos.py
+++ 1.6-chaos.py
@@ -4,7 +4,8 @@
 def main():
     print("The program illustrates a chaotic function")
     x = eval(input("Enter the first number between 0 and 1: "))
-    for i in range (10):
+    n = eval(input("How many numbers should I print? "))
+    for i in range (n):
         x = 3.9 * x * (1-x)
         print(x)
 
